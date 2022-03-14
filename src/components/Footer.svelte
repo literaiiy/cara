cfd3625e1202212c7010528ef09a6bab5cba44c9
Ethan Yim <liddery69@gmail.com>
2022-03-13 18:36:27 -0700

    first commit with sveltekit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,15 @@
+<script>
+  const year = new Date().getFullYear();
+</script>
+
+<footer>
+  Copyright © {year} <a href="">literaiiy</a>. Built with <a href="">SvelteKit</a> & powered by <a href="">Vercel</a>.
+</footer>
+
+<style>
+  footer {
+
+    font-family: 'Courier New', Courier, monospace;
+    background-color: lightgray;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Footer from "../components/Footer.svelte";
+</script>
+
+<main>
+  <h1>literaiiy.me</h1>
+  <p>Welcome to literaiiy.me v4 (Project Cara).</p>
+</main>
+<Footer />
+
+<style>
+  main {
+    font-family: 'Courier New', Courier, monospace
+  }
+</style>
